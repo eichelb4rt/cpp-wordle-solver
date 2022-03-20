13d0c2b90c878f375a59f675a40b3eb46a58ea86
eichelb4rt <maurice.wenig@gmail.com>
2022-03-20 18:51:03 +0100

    fix: testing

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd build 2> /dev/null || echo "build first!"; exit 1
-make test
+cd build 2>/dev/null || { echo "build first!"; exit 1; }
+ctest -j10 -C Debug -T test --output-on-failure
